9f2ba1a15dd6a0ea881f9cec8c3ff8586bd3da89
navff <var@33kita.ru>
2020-01-07 09:51:52 +0300

    __str__ for Question

--- answers/question_module/Question.py
+++ answers/question_module/Question.py
@@ -5,3 +5,7 @@ class Question(models.Model):
     text = models.TextField()
     date = models.DateTimeField()
     author = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.author + ' — ' + self.text[:30]
+
